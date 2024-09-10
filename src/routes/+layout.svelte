680ca0961781c7473a3971ad9f8f0f9e96eaa02d
vachan-maker <vachan@tutanota.com>
2024-09-10 22:04:22 +0530

    Added global layout and css file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Title from "../lib/title.svelte";
+    import '../styles/styles.css'
+
+
+</script>
+<header>
+    <Title title = "Title from layout"/>
+</header>
+<main>
+    <!-- page content-->
+    <slot></slot>
+    <footer>
+        <p>Copyright 2024</p>
+    </footer>
+</main>
